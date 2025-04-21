c2948b0493431aa873258f86456300058e8b226a
Brittany Halterman <halterma@gmail.com>
2025-04-21 12:37:47 -0400

    removeFromArray: pass 'removes a single value' test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, elementToRemove) {
+  newArray = [];
+  for (element of array) {
+    if (element !== elementToRemove) {
+      newArray.push(element);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line
