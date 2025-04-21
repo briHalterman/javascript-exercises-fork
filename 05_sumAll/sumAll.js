640aee11fc7bcccde76f8655e0c0d53e3294f9ae
Brittany Halterman <halterma@gmail.com>
2025-04-21 13:20:42 -0400

    sumAll: pass 'sums numbers within the range' test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function (firstAddend, lastAddend) {
+  let sum = 0;
 
+  for (let i = firstAddend; i <= lastAddend; i++) {
+    sum = sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
