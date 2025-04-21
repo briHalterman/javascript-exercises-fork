67653c281a242afde447b87ac405652d3c9748dd
Brittany Halterman <halterma@gmail.com>
2025-04-21 13:31:53 -0400

    sumAll: pass 'returns ERROR with non-number parameters' test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,7 +3,14 @@ const sumAll = function (num1, num2) {
   let firstAddend;
   let lastAddend;
 
-  if (num1 < 0 || num2 < 0 || num1 % 1 !== 0 || num2 % 1 !== 0) {
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    num1 % 1 !== 0 ||
+    num2 % 1 !== 0 ||
+    typeof num1 !== 'number' ||
+    typeof num2 !== 'number'
+  ) {
     return 'ERROR';
   }
 
