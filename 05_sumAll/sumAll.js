c3c640cea4fad6c211719a7268717cb2bd25f9e6
Brittany Halterman <halterma@gmail.com>
2025-04-21 13:24:46 -0400

    sumAll: pass 'works with larger number first' test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function (firstAddend, lastAddend) {
+const sumAll = function (num1, num2) {
   let sum = 0;
+  let firstAddend;
+  let lastAddend;
+
+  if (num1 < num2) {
+    firstAddend = num1;
+    lastAddend = num2;
+  } else {
+    firstAddend = num2;
+    lastAddend = num1;
+  }
 
   for (let i = firstAddend; i <= lastAddend; i++) {
     sum = sum += i;
