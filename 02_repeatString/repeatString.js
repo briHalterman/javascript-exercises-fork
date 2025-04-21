4ca5201ae3c5036053de66321f733b45fb6371a1
Brittany Halterman <halterma@gmail.com>
2025-04-21 12:13:35 -0400

    repeatString: pass 'returns ERROR with negative numbers' test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,14 @@
 const repeatString = function (string, num) {
   let finalString = '';
 
-  for (let i = 1; i <= num; i++) {
-    finalString += string;
+  if (num < 0) {
+    return 'ERROR';
+  } else {
+    for (let i = 1; i <= num; i++) {
+      finalString += string;
+    }
+    return finalString;
   }
-  return finalString;
 };
 
 // Do not edit below this line
