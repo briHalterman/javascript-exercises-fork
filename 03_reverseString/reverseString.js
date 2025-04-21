80956c74c527fd0082948a0274a22554486e3b0a
Brittany Halterman <halterma@gmail.com>
2025-04-21 12:28:18 -0400

    reverseString: pass 'reverses single word' test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let array = [...string];
+  let reversedArray = array.reverse();
+  let reversedString = reversedArray.join('');
+  return reversedString;
 };
 
 // Do not edit below this line
