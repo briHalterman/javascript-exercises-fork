329b6726fdbc30430688f8859b16bf4c03c6287f
Brittany Halterman <halterma@gmail.com>
2025-04-24 19:50:48 -0400

    Implement factorial function, passing all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,7 +18,16 @@ const power = function (a, b) {
   return a ** b;
 };
 
-const factorial = function () {};
+const factorial = function (num) {
+  // Did you know? 0 factorial does indeed equal 1! Who knew?!
+
+  let product = 1;
+
+  for (i = num; i > 0; i--) {
+    product *= i;
+  }
+  return product;
+};
 
 // Do not edit below this line
 module.exports = {
