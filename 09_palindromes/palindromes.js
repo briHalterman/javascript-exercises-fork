dfed20bc1526d428f47e6ceb7e1597de5aabaf09
Brittany Halterman <halterma@gmail.com>
2025-04-24 20:06:08 -0400

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const string = str.toLowerCase().replace(/[^a-z0-9]/g, "");
 
+  let gnirts = string.split("").reverse().join("");
+
+  return string === gnirts;
 };
 
 // Do not edit below this line
