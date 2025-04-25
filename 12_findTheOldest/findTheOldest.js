779828b1ee1febd4b83687c969bbe0460523d2ce
Brittany Halterman <halterma@gmail.com>
2025-04-25 03:25:26 -0400

    Complete findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let age = 0;
+  const calculateAge = (person) => {
+    const recentYear = person.yearOfDeath || new Date().getFullYear();
+    const age = recentYear - person.yearOfBirth;
+    return age;
+  };
 
+  const sorted = people.toSorted(
+    (person, nextPerson) =>
+      calculateAge(nextPerson) - calculateAge(person)
+  );
+  return sorted[0];
 };
 
 // Do not edit below this line
